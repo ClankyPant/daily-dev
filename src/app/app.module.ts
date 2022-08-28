3cfddc10d56d17ab80918926136a81c87a20b985
Luis <luishenriquealbino@gmail.com>
2022-08-27 23:52:27 -0300

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MatCardModule } from '@angular/material/card';
+import { DevCardComponent } from './dev-card/dev-card.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
+  declarations: [AppComponent, NavBarComponent, DevCardComponent],
   imports: [
     BrowserModule,
+    MatFormFieldModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
